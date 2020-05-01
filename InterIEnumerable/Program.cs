f77c4d5392b9f1b0729f5d937899c4c335e0d6ab
oldraftsmen <oldraftsmen@gmail.com>
2020-05-01 17:33:01 +0300

    Initial commit

---
+++ InterIEnumerable/Program.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace InterIEnumerable
+{
+    class Program
+    {
+        static void Main()
+        {
+            UserCollection myCollection = new UserCollection();
+            List<int> l1 = new List<int> { 1, 2, 3, 4, 5 };
+            foreach (var item in l1)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            foreach (var item in l1)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            // Используем foreach, для обращения к каждому объекту Element внутри массива myCollection. 
+            foreach (Element element in myCollection)
+            {
+                Console.WriteLine("Name: {0}  Field1: {1} Field2: {2}", element.Name, element.Field1, element.Field2);
+            }
+
+            //myCollection.Reset(); // Убрать комментарий для проверки.
+
+            Console.Write(new string('-', 29) + "\n");
+
+            // Используем foreach, для повторного обращения к каждому объекту Element внутри массива myCollection.
+            foreach (Element element in myCollection)
+            {
+                Console.WriteLine("Name: {0}  Field1: {1} Field2: {2}", element.Name, element.Field1, element.Field2);
+            }
+
+            Console.Write(new string('-', 29) + "\n");
+
+
+            // ----------------------------------------------------------------------------------------------------------------------
+            // Так работает foreach.
+
+            UserCollection myElementsCollection = new UserCollection();
+
+            // foreach - приводит коллекцию к интерфейсному типу IEnumerable.
+            IEnumerable enumerable = myElementsCollection as IEnumerable;
+
+            // foreach - приводит коллекцию к интерфейсному типу вызывая метод - GetEnumerator().            
+            IEnumerator enumerator = enumerable.GetEnumerator();
+
+
+            while (enumerator.MoveNext()) // Перемещаем курсор на 1 шаг вперед (с -1 на 0) и т.д.
+            {
+                Element element = enumerator.Current as Element;
+
+                Console.WriteLine("Name: {0}  Field1: {1} Field2: {2}", element.Name, element.Field1, element.Field2);
+            }
+
+
+            enumerator.Reset();
+
+            // Задержка.
+            Console.ReadKey();
+        }
+    }
+}
