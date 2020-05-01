7fcc5143c0873fc881e72787e2ddccc143c9ae8d
oldraftsmen <oldraftsmen@gmail.com>
2020-05-01 19:58:00 +0300

    2

--- InterIEnumerable/UserCollection/UserCollection.cs
+++ InterIEnumerable/UserCollection/UserCollection.cs
@@ -14,8 +14,8 @@ namespace InterIEnumerable
             elementsArray = new Element[4];
             elementsArray[0] = new Element("A", 1, 10);
             elementsArray[1] = new Element("B", 2, 20);
-            elementsArray[2] = new Element("C", 3, 30);
-            elementsArray[3] = new Element("D", 4, 40);
+            elementsArray[2] = new Element("C", 3, 80);
+            elementsArray[3] = new Element("D", 4, 90);
         }
         
         // ”казатель текущей позиции элемента в массиве.
